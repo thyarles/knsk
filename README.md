3de1bfd5aa9bbd1848c4c9a493df4abf4fc6b91f
Charles Santos <charles.santos@mpt.mp.br>
2019-10-30 15:34:20 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# knsk - Kubernetes namespace killer
+
+This tool is aimed to kill namespaces that stuck in Terminating mode after you try to delete it.
+
+# Use
+
+#### Just download and call the scrip
+     sh knsk.sh 
---
+++ knsk.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# --------------------------------------------------------------------------
+#
+# knsk.sh
+#
+# This script kill Kubernetes' namespaces that stuck in Terminanting status
+#
+#                                                            Charles Santos 
+#                                                        thyarles@gmail.com
+#
+# --------------------------------------------------------------------------
+
+set -eu
+
+# Set variables
+k=kubectl
+t=$($k -n default describe secret $($k -n default get secrets | grep default | cut -f1 -d ' ') | grep -E '^token' | cut -f2 -d':' | tr -d '\t' | tr -d ' ') || echo Error to get the token
+
+# Get stuck namespaces
+namespace=$($k get ns | grep Terminating | cut -f1 -d ' ')
+
+if [ x$namespace != x ]; then
+  # start the kubeclt proxy
+  $k proxy > /dev/null 2>&1 &
+  k_pid=$!
+  echo $k_pid
+else
+  echo No namespage in Terminating status found
+  exit 0
+fi
+
+# Remove stuck namespaces
+for n in $namespace
+do
+  echo "Force finish of $n... "
+  j=/tmp/$n.json
+  $k get ns $n -o json > $j 
+  sed -i s/\"kubernetes\"//g $j 
+  curl -X PUT --data-binary @$j http://localhost:8001/api/v1/namespaces/$n/finalize -H "Content-Type: application/json" --header "Authorization: Bearer $t" --insecure
+  echo -n "done!"
+done
+
+# Kill kubectl proxy
+kill $k_pid 
